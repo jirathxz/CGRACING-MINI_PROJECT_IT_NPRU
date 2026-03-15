4202339c4999055258acfda2c5770a8d9631bcca
jirathx. <99694384+jirathxz@users.noreply.github.com>
2026-03-16 02:47:27 +0700

    Release 1.0

---
+++ auth.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    const currentPath = window.location.pathname;
+
+    if (isLoggedIn == null) {
+        localStorage.setItem("isLoggedIn", "false");
+    }
+
+    if (isLoggedIn !== "true") {
+
+        const nav_page = document.getElementById("nav_page");
+        if (nav_page) {
+            nav_page.style.display = "none";
+        }
+
+        console.log("ระบบ: ไม่ได้ล็อกอิน");
+
+        if (currentPath !== "/login.html" && currentPath !== "/register.html") {
+            console.log("ไม่อนุญาตให้เข้าถึงหน้า: " + currentPath + " กำลังพากลับไปหน้าเข้าสู่ระบบ...");
+            window.location.href = "/login.html";
+        }
+    } else { 
+        if (currentPath == "/login.html" || currentPath == "/register.html") {
+            window.location.href = "/index.html";
+        }
+    }
+});
